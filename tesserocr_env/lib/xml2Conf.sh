1adccd4fea7b372bf4eb39c9c9bde9dc7035cb4e
Michael Fong <michaelfong2017@gmail.com>
2020-08-01 21:17:13 +0800

    Deleted .gitignore so that packaging files like lib can be included.

---
+++ tesserocr_env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/michael/opt/anaconda3/envs/tesserocr_env/lib"
+XML2_LIBS="-lxml2 -L/Users/michael/opt/anaconda3/envs/tesserocr_env/lib -lz -L/Users/michael/opt/anaconda3/envs/tesserocr_env/lib -llzma -lpthread -L/Users/michael/opt/anaconda3/envs/tesserocr_env/lib -liconv -L/Users/michael/opt/anaconda3/envs/tesserocr_env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/michael/opt/anaconda3/envs/tesserocr_env/include/libxml2 -I/Users/michael/opt/anaconda3/envs/tesserocr_env/include"
+MODULE_VERSION="xml2-2.9.10"
+
